f631bf461129f4da9391bb01e15a29e73d5b2b92
sargeant45 <ethanarterberry@gmail.com>
2015-04-25 22:56:29 -0700

    Start solution and add Unlicense and Readme files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# A Quiet Adventure
+The curiously different text adventure by [Ethan Arterberry](http://ethanarterberry.com). A WIP being written in C# .NET.
---
+++ a-quiet-adventure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_quiet_adventure
+{
+    class Quiet_Adventure
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
